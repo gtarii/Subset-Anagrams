84f67805a313e0444876e0c4983ed5c053846b55
gtarii <ari.howland@gmail.com>
2021-08-23 02:04:41 +1000

    -Unit test for Identifier.GenerateAlphabetPrimeDictionary -Unit test for Identifier.GetProductPrimeValue -GetInput method for Solver -Some updates to Main method for Solver

--- Subset Anagrams Tests/IdentiferTests.cs
+++ Subset Anagrams Tests/IdentiferTests.cs
@@ -1,6 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.IO;
 using System;
+using Subset_Anagrams;
 
 namespace Subset_Anagrams_Tests
 {
@@ -9,7 +10,45 @@ namespace Subset_Anagrams_Tests
     {
         private const int numberOfPrimesTested = 10;
         private static readonly int[] firstTenPrimes = new int[] { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 }; // First 10 primes
+        private static readonly string[] testCharacters = new string[] {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"}; // character placements in english alphabet: s-19th, h-8th, e-5th, p-16th
+
         [TestMethod]
+        // Test the GetPrimeProductVal method from the Identifier Class
+        public void Test_GetPrimeProductVal()
+        {
+            var expectedPrimeProduct = 1; // calculate our expected prime product by taking the product of our array of test primes
+            foreach (int p in firstTenPrimes)
+            {
+                expectedPrimeProduct *= p;
+            }
+            Identifier identifier = new Identifier();
+            var primeProduct = Subset_Anagrams.Identifier.GetProductPrimeValue(testCharacters);
+            Assert.AreEqual(expectedPrimeProduct, primeProduct);
+        }
+
+        [TestMethod]
+        // Test the GenerateAlphabetPrimeDictionary method from the Identifier Class
+        public void TestVal_GenerateAlphabetPrimeDictionary()
+        {
+            var testDict = Subset_Anagrams.Identifier.GenerateAlphabetPrimeDictionary();
+            var test_keys = testCharacters;
+
+            // we test the generated dictionary by giving it keys and summing the values returned. We compare this sum to the expected sum.
+            int test_sum = 0;
+            foreach (string key in test_keys)
+            {
+                test_sum = test_sum + testDict[key];
+            }
+            var expected_sum = 0; // expected sum is the sum of our prime array
+            foreach (int p in firstTenPrimes)
+            {
+                expected_sum += p;
+            }
+            Assert.AreEqual(expected_sum, test_sum);
+        }
+
+        [TestMethod]
+        // Test the GeneratePrimes method from the Identifier Class
         public void Test_GeneratePrimes()
         {
             var expected = firstTenPrimes;
---
+++ Subset Anagrams/Solver.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+// Objective of this program is recieve user input of a string comprised of letters and return all anagrams of the subset of letters
+namespace Subset_Anagrams
+{
+    // The Solver class is the primary class of the program.
+    class Solver
+    {
+        public static void Main(string[] args)
+        {
+            // set up variables
+            string fileLocation = @"C:\Users\ariho\VisualStudio\source\repos\Subset Anagrams\data\wordlist.10000.txt";
+            String[] wordDictionary = ReadFromFileLSV(@fileLocation);
+
+            // create an instance of our identifer class
+            var identifer = new Identifier();
+
+        }
+
+
+        // Method for requesting a set of characters from the user. We clean whitespace, uncapitalise all and remove non alphabetical characters
+        public static char[] GetInput()
+        {
+            // explain rules for input to user
+            String inputRulesString = "please input a word or some letters, no numbers or special characters";
+            Console.WriteLine(inputRulesString);
+            
+            // get user input as string
+            var input = Console.ReadLine();
+
+            // get only desired characters from input, i.e. no whitespace or special characters
+            var onlyLetters = new String(input.Where(Char.IsLetter).ToArray());
+            return onlyLetters.ToCharArray();
+        }
+
+
+        // Method that reads a .txt file containing a dictionary of words in line separated value (LSV) format and stores this data in an array.
+        public static string[] ReadFromFileLSV(string fileLocation)
+        {
+            return System.IO.File.ReadAllLines(@fileLocation);
+        }
+
+
+        // Method to merge an array of words and an equal length array of integers together
+
+    }
+}
